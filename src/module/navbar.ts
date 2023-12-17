3af5306d2fc8d2d233ee49c18a3876f4ebd4e539
Teddy Roncin <teddy.roncin@proton.me>
2023-12-17 14:20:36 +0100

    fix: cleaned code

--- src/module/navbar.ts
+++ src/module/navbar.ts
@@ -19,23 +19,20 @@ export const userSlice = createSlice({
       for (let i = 0; i < (action.payload.parents.length > 0 ? location.length : 0); i++) {
         const index: number = list.findIndex(({ name, submenus }) => name === location[i] && submenus != null);
         if (index < 0) {
-          list = null;
-          break;
+          return;
         }
         list = list[index].submenus!;
       }
-      if (list != null) {
-        if (list.findIndex(({ name }) => name === action.payload.item.name) >= 0) return; // an item already has this name
-        if (action.payload.before != null) {
-          let index = list.findIndex(({ name }) => name === action.payload.before);
-          if (index < 0) index = list.length;
-          list.splice(index, 0, action.payload.item);
-        } else if (action.payload.after != null) {
-          let index = list.findIndex(({ name }) => name === action.payload.after);
-          if (index < 0) index = list.length - 1;
-          list.splice(index + 1, 0, action.payload.item);
-        } else list.push(action.payload.item);
-      }
+      if (list.findIndex(({ name }) => name === action.payload.item.name) >= 0) return; // an item already has this name
+      if (action.payload.before != null) {
+        let index = list.findIndex(({ name }) => name === action.payload.before);
+        if (index < 0) index = list.length;
+        list.splice(index, 0, action.payload.item);
+      } else if (action.payload.after != null) {
+        let index = list.findIndex(({ name }) => name === action.payload.after);
+        if (index < 0) index = list.length - 1;
+        list.splice(index + 1, 0, action.payload.item);
+      } else list.push(action.payload.item);
     },
     replaceItem: (state, action: PayloadAction<{ item: MenuItem; search: string }>) => {
       const location = action.payload.search.split(',');
