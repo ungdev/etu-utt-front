0e6baac2cede9ec22f5c93644ffa9f989b44e0fc
Teddy Roncin <teddy.roncin@proton.me>
2024-01-31 15:46:17 +0100

    fix: removed library next-i18next

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import styles from './Navbar.module.scss';
 import Link from 'next/link';
 import { CSSProperties, useRef, useState } from 'react';
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from 'react-i18next';
 
 const menus: Array<
   | { name: string; redirect?: undefined; submenus: { name: string; redirect: string }[] }
