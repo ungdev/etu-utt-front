15ed2319ac99cb09af0eb451fd1d71732dd23276
Teddy Roncin <teddy.roncin@proton.me>
2024-03-23 11:56:56 +0100

    Update src/icons/index.ts
    Co-authored-by: Alban Sdl <35579652+AlbanSdl@users.noreply.github.com>

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -5,6 +5,7 @@ import Home from '@/icons/Home';
 import Menu from '@/icons/Menu';
 import User from '@/icons/User';
 import Users from '@/icons/Users';
+import Star from '@/icons/Star';
 import Trash from '@/icons/Trash';
 
 const Icons = {
@@ -13,6 +14,7 @@ const Icons = {
   Collapse,
   Home,
   Menu,
+  Star,
   Trash,
   User,
   Users,
