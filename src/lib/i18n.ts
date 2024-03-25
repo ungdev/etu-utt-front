0db99e9607c939564892bd4f637eb58011e8c258
Teddy Roncin <teddy.roncin@proton.me>
2024-03-26 00:25:24 +0100

    feat: added a ctrl-k shortcut to go to a certain part of the site easily

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -24,7 +24,7 @@ i18n
       useSuspense: true,
     },
     supportedLngs,
-    ns: ['common', 'login', 'ues', 'homepage'],
+    ns: ['common', 'login', 'ues', 'homepage', 'goTo'],
     preload: ['fr'],
     nsSeparator: ':',
     defaultNS: 'common',
