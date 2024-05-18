b4c807ea54fcd0e98ca32f77a13e384a57eeeea3
Teddy Roncin <teddy.roncin@proton.me>
2024-05-18 12:03:41 +0200

    nice bug

--- src/components/homeWidgets/WidgetLayout.tsx
+++ src/components/homeWidgets/WidgetLayout.tsx
@@ -1,5 +1,5 @@
 import styles from './WidgetLayout.module.scss';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 
 export function WidgetLayout({
   title,
@@ -12,11 +12,40 @@ export function WidgetLayout({
   children?: ReactNode;
   className?: string;
 }) {
+  const ref = useRef<HTMLDivElement>();
+  useEffect(() => {
+    if (!ref.current) return;
+    if (title !== 'Trombinoscope') return;
+    console.log(ref.current);
+    //for (let i = 0; i < ref.current?.childElementCount; i++) {
+    //  ref.current?.childNodes[i].scale = `${ref.current.clientWidth / ref.current.scrollWidth}`;
+    //}
+    // ref.current.style.scale = `${ref.current?.clientWidth / ref.current?.scrollWidth}`;
+  }, [ref.current]);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(() => {
+      const scaleX = ref.current!.clientWidth / ref.current!.scrollWidth;
+      const scaleY = ref.current!.clientHeight / ref.current!.scrollHeight;
+      if (scaleX < scaleY) {
+        ref.current!.style.width = `${(scaleY / scaleX) * 100}%`;
+        ref.current!.style.height = '';
+      } else {
+        ref.current!.style.height = `${(scaleX / scaleY) * 100}%`;
+        ref.current!.style.width = '';
+      }
+      ref.current!.style.scale = `${Math.min(ref.current!.clientWidth / ref.current!.scrollWidth, ref.current!.clientHeight / ref.current!.scrollHeight)}`;
+    });
+    resizeObserver.observe(ref.current!);
+    return () => resizeObserver.disconnect();
+  }, []);
   return (
     <div className={styles.widget}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.subtitle}>{subtitle}</p>
-      <div className={className}>{children}</div>
+      <div className={styles.resizableWrapper} ref={ref}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.subtitle}>{subtitle}</p>
+        <div className={className}>{children}</div>
+      </div>
     </div>
   );
 }
