96df7e0f33a7dc1627ccf92898e14c0c6fc95bc7
Teddy Roncin <teddy.roncin@proton.me>
2025-09-19 12:01:19 +0200

    Removed useless checks for null-ness in ExamList.tsx

--- src/components/ues/ExamList.tsx
+++ src/components/ues/ExamList.tsx
@@ -47,7 +47,7 @@ export default function ExamList({
       <div className={styles.exams}>
         <h2>{t('ues:detailed.annals.title')}</h2>
         <div className={styles.list}>
-          {annals?.length
+          {annals.length
             ? Object.entries(Object.groupBy(annals, (annal) => annal.semesterId)).map(([semester, annals]) => (
                 <div className={styles.semester} key={semester}>
                   <h3>{semester}</h3>
@@ -85,7 +85,7 @@ export default function ExamList({
         </div>
         <Button
           className={styles.sendButton}
-          disabled={!annalTypes?.length || !annalSemesters?.length}
+          disabled={!annalTypes.length || !annalSemesters.length}
           onClick={() => setAnnalUploaderOpen(true)}>
           {t('ues:detailed.annals.send')}
         </Button>
