9a81fe12eb49021484e16e65072c950fbdedba9d
AlbanSdl <alban.delavoreille@free.fr>
2025-10-03 22:54:43 +0200

    feat: add membership list

--- public/locales/fr/assos.json.ts
+++ public/locales/fr/assos.json.ts
@@ -2,8 +2,12 @@
 // For more information, check the common.json.ts file
 
 export default {
-  "browser": "UTT Travail",
-  "filter.search": "Recherche dans le guide des assos",
-  "filter.search.title": "Recherche dans le guide des assos"
+  browser: 'UTT Travail',
+  'filter.search': 'Recherche dans le guide des assos',
+  'filter.search.title': 'Recherche dans le guide des assos',
+  'member.since': 'Depuis ',
+  'member.old.from': 'Entre ',
+  'member.old.to': ' et ',
+  'member.old.display': 'Afficher les anciens',
+  'member.old.hide': 'Masquer les anciens',
 } as const;
-
