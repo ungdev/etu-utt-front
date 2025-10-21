9c17a64a9fd6ffd5530324b5faa1125004b171c6
AlbanSdl <alban.delavoreille@free.fr>
2025-10-21 03:49:13 +0200

    fix: add ts-expect-error for global.scss import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// @ts-expect-error global.scss does not match the *.module.scss pattern
 import '@/global.scss';
 import Providers from '@/lib/Providers';
 import React, { ReactNode, Suspense } from 'react';
