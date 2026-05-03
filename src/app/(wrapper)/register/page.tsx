c1e85458a6c419a9dba8c6ba7502e5c4b50ec19b
Arthur Dodin <dodinarthur@gmail.com>
2026-05-03 23:18:14 +0200

    fix(auth): return 404 on /auth/register if not in dev mode

--- src/app/(wrapper)/register/page.tsx
+++ src/app/(wrapper)/register/page.tsx
@@ -2,8 +2,14 @@
 import RegisterForm from '@/components/auth/RegisterForm';
 import styles from '@/app/(wrapper)/login/style.module.scss';
 import Page from '@/components/utilities/Page';
+import { notFound } from 'next/navigation';
+import { isDevEnv } from '@/utils/environment';
 
 export default function RegisterPage() {
+  if (!isDevEnv() || true) {
+    return notFound();
+  }
+
   return (
     <Page hasNavbar={true} id="register-page" className={styles.loginPage} noWrapperPadding={true}>
       <RegisterForm />
